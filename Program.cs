e94910e08ad10e684987da9100e880fcd99f1160
robbfreitas <robertocompromissos@gmail.com>
2022-12-16 18:33:45 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+/*
+Entrada de dados
+    Array
+    ReadLine()
+Operação
+    For()
+    Media
+Saída de Dados
+    WriteLine
+*/
+
+namespace VetoresEmCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULE A MÉDIA DE ALTURA");
+            Console.WriteLine();
+
+            Console.Write("Informe quantas alturas serão calculadas: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Informe uma altura: ");
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+            }
+
+            double sum = 0.0;
+
+            for(int i = 0; i < n; i++)
+            {
+                sum += vect[i];
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("Average height: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
